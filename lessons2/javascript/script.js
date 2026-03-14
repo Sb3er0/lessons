69b70c2c0814165581b7846d00697a48a29b4223
Sb3er0 <98053803+Sb3er0@users.noreply.github.com>
2026-03-14 14:55:14 +0100

    Delete lessons2 directory

--- lessons2/javascript/script.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-// Тестовый вариант, он ни на что не влияет, просто для проверки работоспособности TypeScript в проекте
-let quantity = parseInt(prompt(`Введите количество товара`, `10`));
-let units = parseFloat(prompt(`Введите цену единицы товара`, `100`));
-const vat = 5;
-let sum = quantity * units;
-let vatValue = sum * vat / 100;
-document.write(`Сумма НДС(${vat}%) составляет: ${vatValue} грн.`);
-console.log(`Сумма НДС(${vat}%) составляет: ${vatValue} грн.`);
